5020de9bcf22df9743b12d1ad8d21bb55be13e79
Cheryl K <cherylkelson@icloud.com>
2019-03-06 14:35:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ class App < Sinatra::Base
 
   # Add your post route and action below
   post 'food' do
+<<<<<<< HEAD
     params.to_s
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
     end
@@ -18,5 +19,15 @@ class App < Sinatra::Base
   # :favorite_food => "Green Eggs and Ham"
   # }
 
+=======
+  params = {
+  :name => "Sam",
+  :favorite_food => "Green Eggs and Ham"
+  }
+  params.to_s
+
+
+ end
+>>>>>>> 7472b5fedebe30afa3ca9b715e9a514746d5e0d3
 
  end
